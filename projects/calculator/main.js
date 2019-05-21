2b33d0eeff7b603e27eb60f217f76c4d5dcc7baa
David Mora <David.mora@codeimmersives.com>
2019-05-21 11:07:59 -0400

    added global variables

--- projects/calculator/main.js
+++ projects/calculator/main.js
@@ -1,22 +1,27 @@
 window.onload = init;
 
+let storeValue = 0;
+let secondValue = 0;
+
 function init() {
     let buttons = document.querySelector(".calculatorKeys");
-    buttons.addEventListener('click', calculate)  
+    buttons.addEventListener('click', display)  
 }
 
-function calculate() {
+function display() {
     event.preventDefault();
     
-    let e = event.target.innerText;
-    
+    let e = event.target;
+
     let display = document.querySelector('.calculatorDisplay');
 
     if(e <= 9 || display.innerText.includes('.') === false) {
         if(display.innerText === '0') {
-            display.innerText = e;
-        } else{
-            display.innerText += e;
+            display.innerText = e.innerText;
+            storeValue += e.innerText;
+        } else {
+            display.innerText += e.innerText;
+            storeValue += e.innerText;
         }
     }
 }
